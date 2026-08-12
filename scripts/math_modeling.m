74b9179f68b115542022faad925c0c17ce49a385
JNemune <JNemune2002@gmail.com>
2026-08-12 12:33:54 +0330

    prepared non-linear equations for MPC controller, check the equations with matlab

--- scripts/math_modeling.m
+++ scripts/math_modeling.m
@@ -1,3 +1,4 @@
+%% lagrange
 clear; clc; close all;
 
 syms x(t) l(t) theta(t)
@@ -28,6 +29,7 @@ simplify(expand(eq1));
 eq2 = diff(diff(L, diff(theta, t)), t) - diff(L, theta) == Qt;
 simplify(expand(eq2));
 
+%% linear state space
 clear; clc; close all;
 
 syms x__ x_ theta__ theta_ x theta M m g F1 F2 bp bc l0 D
@@ -67,5 +69,66 @@ Dm = [
       [0, 0];
       ];
 
-X = [x; x_; theta, theta_];
-U = [F1, F2];
+X = [x; x_; theta; theta_];
+U = [F1; F2];
+
+X_ = A * X + B * U;
+Y = C * X + Dm * U;
+
+%% non-linear state space
+clear; clc; close all;
+
+syms x x_ x__ theta theta_ theta__ l l_
+syms F1 F2 l__
+syms M m g bc bp
+assume(M > 0);
+assume(m > 0);
+assume(g > 0);
+assume(bc > 0);
+assume(bp > 0);
+assume(theta > -pi / 2);
+assume(theta < pi / 2);
+assume(l > 0);
+
+x1 = x;
+x2 = x_;
+x3 = theta;
+x4 = theta_;
+x5 = l;
+x6 = l_;
+
+eqns = [
+        (M + m) * x__ + m * l * theta__ * cos(theta) + m * l__ * sin(theta) + 2 * m * l_ * theta_ * cos(theta) - m * l * theta_ ^ 2 * sin(theta) + bc * x_ == F1 + F2;
+        m * l ^ 2 * theta__ + m * l * x__ * cos(theta) + 2 * m * l * l_ * theta_ + m * g * l * sin(theta) + bp * theta_ == 0;
+        ];
+slv = solve(eqns, [x__, theta__]);
+simplify(slv.x__);
+simplify(slv.theta__);
+
+X = [x; x_; theta; theta_; l; l_];
+U = [F1; F2; l__];
+RHS1 = m * x5 * x4 ^ 2 * sin(x3) - 2 * m * x6 * x4 * cos(x3) - bc * x2;
+RHS2 = -2 * m * x5 * x6 * x4 - m * g * x5 * sin(x3) - bp * x4;
+gamma = M + m * sin(x3) ^ 2;
+F = [
+     x2;
+     (x5 * RHS1 - cos(x3) * RHS2) / x5 / (M + m * sin(x3) ^ 2);
+     x4;
+     (-m * x5 * cos(x3) * RHS1 + (M + m) * RHS2) / m / x5 ^ 2 / (M + m * sin(x3) ^ 2);
+     x6;
+     0;
+     ];
+G = [
+     [0, 0, 0];
+     [1 / gamma, 1 / gamma, -m * sin(x3) / gamma];
+     [0, 0, 0];
+     [-cos(x3) / x5 / gamma, -cos(x3) / x5 / gamma, m * sin(x3) * cos(x3) / x5 / gamma];
+     [0, 0, 0];
+     [0, 0, 1];
+     ];
+
+X_ = F + G * U;
+simplify(X_);
+
+simplify(X_(2) - slv.x__);
+simplify(X_(4) - slv.theta__);
