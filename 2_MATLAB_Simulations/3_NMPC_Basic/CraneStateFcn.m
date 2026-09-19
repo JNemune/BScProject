f9a4662816a460e52876b6cb85d672420c99904d
JNemune <JNemune2002@gmail.com>
2026-09-19 12:05:15 +0330

    compelete NMPC design section. replace block diagram image with pdf version, modify simscape and add it's codes to appendix

--- 2_MATLAB_Simulations/3_NMPC_Basic/CraneStateFcn.m
+++ 2_MATLAB_Simulations/3_NMPC_Basic/CraneStateFcn.m
@@ -1,48 +1,48 @@
 function dxdt = CraneStateFcn(x, u)
-    % CraneStateFcn: Continuous-time nonlinear state equations for NMPC
+    % Continuous-time augmented nonlinear model for NMPC
     % States: x = [x; x_dot; theta; theta_dot; l; l_dot]
     % Inputs: u = [F; l_ddot]
 
-    % L_initial = 0.5;
-    M = 1.0; % [kg]
-    bc = 0.1; % [N.s/m]
-    bp = 0.05; % [N.m.s/rad]
-    g = 9.81; % [m/s^2]
-    m = 0.2; % [kg]
+    % System parameters
+    M = 1.0;
+    bc = 0.1;
+    bp = 0.05;
+    g = 9.81;
+    m = 0.2;
 
-    % x1 = x(1); % x
-    x2 = x(2); % x_dot
-    x3 = x(3); % theta
-    x4 = x(4); % theta_dot
-    x5 = x(5); % l
-    x6 = x(6); % l_dot
+    % Extract states and inputs
+    x2 = x(2);
+    x3 = x(3);
+    x4 = x(4);
+    x5 = x(5);
+    x6 = x(6);
 
-    F = u(1);
-    l_ddot = u(2);
+    u1 = u(1);
+    u2 = u(2);
 
+    % Prevent singularity at l = 0
     if x5 < 0.01
         x5 = 0.01;
     end
 
-    RHS1 = m * x5 * x4 ^ 2 * sin(x3) - 2 * m * x6 * x4 * cos(x3) - bc * x2;
+    % Exact RHS terms from symbolic derivation
+    RHS1 = u1 - bc * x2 - m * u2 * sin(x3) - 2 * m * x6 * x4 * cos(x3) + m * x5 * x4 ^ 2 * sin(x3);
     RHS2 = -2 * m * x5 * x6 * x4 - m * g * x5 * sin(x3) - bp * x4;
-    gamma = M + m * sin(x3) ^ 2;
+    RHS = [RHS1; RHS2];
 
+    % Exact inertia matrix M(x,u)
+    M_mat = [M + m, m * x5 * cos(x3);
+             m * x5 * cos(x3), m * x5 ^ 2];
+
+    % Compute accelerations
+    acc = M_mat \ RHS;
+
+    % Augmented state derivatives
     dxdt = zeros(6, 1);
-
     dxdt(1) = x2;
-
-    dxdt(2) = (x5 * RHS1 - cos(x3) * RHS2) / (x5 * gamma) ...
-        + (F) / gamma ...
-        - (m * sin(x3) / gamma) * l_ddot;
-
+    dxdt(2) = acc(1);
     dxdt(3) = x4;
-
-    dxdt(4) = (-m * x5 * cos(x3) * RHS1 + (M + m) * RHS2) / (m * x5 ^ 2 * gamma) ...
-        - (cos(x3) / (x5 * gamma)) * (F) ...
-        + (m * sin(x3) * cos(x3) / (x5 * gamma)) * l_ddot;
-
+    dxdt(4) = acc(2);
     dxdt(5) = x6;
-
-    dxdt(6) = l_ddot;
+    dxdt(6) = u2;
 end
