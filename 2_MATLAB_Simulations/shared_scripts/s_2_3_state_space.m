efce2f4c83596831940c4dec200a59c6a8e6a684
JNemune <JNemune2002@gmail.com>
2026-09-22 17:21:01 +0330

    refactore state space base on augmented satates

--- 2_MATLAB_Simulations/shared_scripts/s_2_3_state_space.m
+++ 2_MATLAB_Simulations/shared_scripts/s_2_3_state_space.m
@@ -1,41 +1,44 @@
 clear; clc; close all;
 
 % Define symbolic variables for states and inputs
-syms x1 x2 x3 x4
-syms u1 u2 u3 u4
+syms x x_dot theta theta_dot l l_dot
+syms F l_ddot
 syms M m g bc bp l0
 
-X = [x1; x2; x3; x4];
-U = [u1; u2; u3; u4];
+X = [x; x_dot; theta; theta_dot; l; l_dot];
+U = [F; l_ddot];
 
-% Inertia matrix M(x,u)
-M_mat = [M + m, m * u4 * cos(x3);
-         m * u4 * cos(x3), m * u4 ^ 2];
-
-disp('--- Inertia Matrix M(x,u) ---');
+% Inertia matrix M(x)
+M_mat = [M + m, m * l * cos(theta);
+         m * l * cos(theta), m * l ^ 2];
+disp('--- Inertia Matrix M(x) ---');
 disp(M_mat);
 
 % Right hand side vector RHS(x,u)
-RHS1 = u1 - bc * x2 - m * u2 * sin(x3) - 2 * m * u3 * x4 * cos(x3) + m * u4 * x4 ^ 2 * sin(x3);
-RHS2 = -2 * m * u4 * u3 * x4 - m * g * u4 * sin(x3) - bp * x4;
+RHS1 = F - bc * x_dot - m * l_ddot * sin(theta) ...
+    - 2 * m * l_dot * theta_dot * cos(theta) ...
+    + m * l * theta_dot ^ 2 * sin(theta);
+RHS2 = -2 * m * l * l_dot * theta_dot ...
+    - m * g * l * sin(theta) ...
+    - bp * theta_dot;
 RHS = [RHS1; RHS2];
-
 disp('--- RHS Vector ---');
 disp(RHS);
 
 % Determinant of M_mat
 det_M = simplify(det(M_mat));
-disp('--- Determinant of M(x,u) ---');
+disp('--- Determinant of M(x) ---');
 disp(det_M);
 
 % Non-linear state-space (X_dot = f)
 acc = M_mat \ RHS;
-f = [x2;
+f = [x_dot;
      acc(1);
-     x4;
-     acc(2)];
+     theta_dot;
+     acc(2);
+     l_dot;
+     l_ddot];
 f = simplify(f);
-
 disp('--- Non-linear State-Space Equations (f) ---');
 disp(f);
 
@@ -53,56 +56,67 @@ disp('--- Jacobian Matrix B(x,u) ---');
 disp(B_jac);
 
 % Equilibrium point
-X_eq = [0; 0; 0; 0];
-U_eq = [0; 0; 0; l0];
+X_eq = [0; 0; 0; 0; l0; 0];
+U_eq = [0; 0];
 
 % Linearized matrices at equilibrium point
 A_lin = subs(A_jac, [X; U], [X_eq; U_eq]);
 B_lin = subs(B_jac, [X; U], [X_eq; U_eq]);
+A_lin = simplify(A_lin);
+B_lin = simplify(B_lin);
 
 disp('--- Linearized Matrix A at Equilibrium ---');
 disp(A_lin);
-
 disp('--- Linearized Matrix B at Equilibrium ---');
 disp(B_lin);
 
-% --- Inertia Matrix M(x, u) ---
-% [M + m, m * u4 * cos(x3)]
-% [m * u4 * cos(x3), m * u4 ^ 2]
+% --- Inertia Matrix M(x) ---
+% [M + m, l * m * cos(theta)]
+% [l * m * cos(theta), l ^ 2 * m]
 
 % --- RHS Vector ---
-% m * u4 * sin(x3) * x4 ^ 2 - 2 * m * u3 * cos(x3) * x4 + u1 - bc * x2 - m * u2 * sin(x3)
-% - bp * x4 - 2 * m * u3 * u4 * x4 - g * m * u4 * sin(x3)
+% l * m * sin(theta) * theta_dot ^ 2 - 2 * l_dot * m * cos(theta) * theta_dot + F - bc * x_dot - l_ddot * m * sin(theta)
+% - bp * theta_dot - 2 * l * l_dot * m * theta_dot - g * l * m * sin(theta)
 
-% --- Determinant of M(x, u) ---
-% m * u4 ^ 2 * (- m * cos(x3) ^ 2 + M + m)
+% --- Determinant of M(x) ---
+% l ^ 2 * m * (- m * cos(theta) ^ 2 + M + m)
 
 % --- Non - linear State - Space Equations (f) ---
-% x2
-% (u1 * u4 + bp * x4 * cos(x3) - bc * u4 * x2 + m * u4 ^ 2 * x4 ^ 2 * sin(x3) - m * u2 * u4 * sin(x3) + g * m * u4 * cos(x3) * sin(x3)) / (u4 * (- m * cos(x3) ^ 2 + M + m))
-% x4
-% - (2 * (M * bp * x4 + bp * m * x4 + (m ^ 2 * u4 ^ 2 * x4 ^ 2 * sin(2 * x3)) / 2 - (m ^ 2 * u2 * u4 * sin(2 * x3)) / 2 + m ^ 2 * u3 * u4 * x4 + m * u1 * u4 * cos(x3) + g * m ^ 2 * u4 * sin(x3) + 2 * M * m * u3 * u4 * x4 - m ^ 2 * u3 * u4 * x4 * cos(2 * x3) + M * g * m * u4 * sin(x3) - bc * m * u4 * x2 * cos(x3))) / (m * u4 ^ 2 * (2 * M + m - m * cos(2 * x3)))
+% x_dot
+% (F * l + bp * theta_dot * cos(theta) - bc * l * x_dot + l ^ 2 * m * theta_dot ^ 2 * sin(theta) - l * l_ddot * m * sin(theta) + g * l * m * cos(theta) * sin(theta)) / (l * (- m * cos(theta) ^ 2 + M + m))
+% theta_dot
+% - (2 * (M * bp * theta_dot + bp * m * theta_dot + (l ^ 2 * m ^ 2 * theta_dot ^ 2 * sin(2 * theta)) / 2 - (l * l_ddot * m ^ 2 * sin(2 * theta)) / 2 + F * l * m * cos(theta) + l * l_dot * m ^ 2 * theta_dot + g * l * m ^ 2 * sin(theta) + 2 * M * l * l_dot * m * theta_dot - l * l_dot * m ^ 2 * theta_dot * cos(2 * theta) + M * g * l * m * sin(theta) - bc * l * m * x_dot * cos(theta))) / (l ^ 2 * m * (2 * M + m - m * cos(2 * theta)))
+% l_dot
+% l_ddot
 
 % --- Jacobian Matrix A(x, u) ---
-% [0, 1, 0, 0]
-% [0, -bc / (- m * cos(x3) ^ 2 + M + m), - (2 * (bp * x4 * sin(x3) - m * u4 ^ 2 * x4 ^ 2 * cos(x3) + m * u2 * u4 * cos(x3) - g * m * u4 * (2 * cos(x3) ^ 2 - 1))) / (u4 * (2 * M + m - m * (2 * cos(x3) ^ 2 - 1))) - (2 * m * cos(x3) * sin(x3) * (u1 * u4 + bp * x4 * cos(x3) - bc * u4 * x2 + m * u4 ^ 2 * x4 ^ 2 * sin(x3) - m * u2 * u4 * sin(x3) + g * m * u4 * cos(x3) * sin(x3))) / (u4 * (- m * cos(x3) ^ 2 + M + m) ^ 2), (2 * m * x4 * sin(x3) * u4 ^ 2 + bp * cos(x3)) / (u4 * (- m * cos(x3) ^ 2 + M + m))]
-% [0, 0, 0, 1]
-% [0, (bc * cos(x3)) / (u4 * (- m * cos(x3) ^ 2 + M + m)), (4 * sin(2 * x3) * (M * bp * x4 + bp * m * x4 + (m ^ 2 * u4 ^ 2 * x4 ^ 2 * sin(2 * x3)) / 2 - (m ^ 2 * u2 * u4 * sin(2 * x3)) / 2 + m ^ 2 * u3 * u4 * x4 + m * u1 * u4 * cos(x3) + g * m ^ 2 * u4 * sin(x3) + 2 * M * m * u3 * u4 * x4 - m ^ 2 * u3 * u4 * x4 * cos(2 * x3) + M * g * m * u4 * sin(x3) - bc * m * u4 * x2 * cos(x3))) / (u4 ^ 2 * (2 * M + m - m * cos(2 * x3)) ^ 2) - (2 * (m * u4 * (2 * cos(x3) ^ 2 - 1) * x4 ^ 2 + 4 * m * u3 * cos(x3) * sin(x3) * x4 - u1 * sin(x3) + g * m * cos(x3) + bc * x2 * sin(x3) - m * u2 * (2 * cos(x3) ^ 2 - 1) + M * g * cos(x3))) / (u4 * (2 * M + m - m * (2 * cos(x3) ^ 2 - 1))), - (2 * (M * bp + bp * m + m ^ 2 * u3 * u4 + 2 * M * m * u3 * u4 - m ^ 2 * u3 * u4 * cos(2 * x3) + m ^ 2 * u4 ^ 2 * x4 * sin(2 * x3))) / (m * u4 ^ 2 * (2 * M + m - m * cos(2 * x3)))]
+% [0, 1, 0, 0, 0, 0]
+% [0, -bc / (- m * cos(theta) ^ 2 + M + m), - (2 * (bp * theta_dot * sin(theta) - l ^ 2 * m * theta_dot ^ 2 * cos(theta) + l * l_ddot * m * cos(theta) - g * l * m * (2 * cos(theta) ^ 2 - 1))) / (l * (2 * M + m - m * (2 * cos(theta) ^ 2 - 1))) - (2 * m * cos(theta) * sin(theta) * (F * l + bp * theta_dot * cos(theta) - bc * l * x_dot + l ^ 2 * m * theta_dot ^ 2 * sin(theta) - l * l_ddot * m * sin(theta) + g * l * m * cos(theta) * sin(theta))) / (l * (- m * cos(theta) ^ 2 + M + m) ^ 2), (2 * m * theta_dot * sin(theta) * l ^ 2 + bp * cos(theta)) / (l * (- m * cos(theta) ^ 2 + M + m)), - (theta_dot * (- m * theta_dot * sin(theta) * l ^ 2 + bp * cos(theta))) / (l ^ 2 * (- m * cos(theta) ^ 2 + M + m)), 0]
+% [0, 0, 0, 1, 0, 0]
+% [0, (bc * cos(theta)) / (l * (- m * cos(theta) ^ 2 + M + m)), (4 * sin(2 * theta) * (M * bp * theta_dot + bp * m * theta_dot + (l ^ 2 * m ^ 2 * theta_dot ^ 2 * sin(2 * theta)) / 2 - (l * l_ddot * m ^ 2 * sin(2 * theta)) / 2 + F * l * m * cos(theta) + l * l_dot * m ^ 2 * theta_dot + g * l * m ^ 2 * sin(theta) + 2 * M * l * l_dot * m * theta_dot - l * l_dot * m ^ 2 * theta_dot * cos(2 * theta) + M * g * l * m * sin(theta) - bc * l * m * x_dot * cos(theta))) / (l ^ 2 * (2 * M + m - m * cos(2 * theta)) ^ 2) - (2 * (l * m * (2 * cos(theta) ^ 2 - 1) * theta_dot ^ 2 + 4 * l_dot * m * cos(theta) * sin(theta) * theta_dot - F * sin(theta) + g * m * cos(theta) + bc * x_dot * sin(theta) - l_ddot * m * (2 * cos(theta) ^ 2 - 1) + M * g * cos(theta))) / (l * (2 * M + m - m * (2 * cos(theta) ^ 2 - 1))), - (2 * (M * bp + bp * m + l * l_dot * m ^ 2 - l * l_dot * m ^ 2 * cos(2 * theta) + l ^ 2 * m ^ 2 * theta_dot * sin(2 * theta) + 2 * M * l * l_dot * m)) / (l ^ 2 * m * (2 * M + m - m * cos(2 * theta))), (2 * M * bp * theta_dot + 2 * bp * m * theta_dot - (l * l_ddot * m ^ 2 * sin(2 * theta)) / 2 + F * l * m * cos(theta) + 2 * l * l_dot * m ^ 2 * theta_dot + g * l * m ^ 2 * sin(theta) + 2 * M * l * l_dot * m * theta_dot - 2 * l * l_dot * m ^ 2 * theta_dot * cos(theta) ^ 2 + M * g * l * m * sin(theta) - bc * l * m * x_dot * cos(theta)) / (l ^ 3 * m * (- m * cos(theta) ^ 2 + M + m)), - (2 * theta_dot) / l]
+% [0, 0, 0, 0, 0, 1]
+% [0, 0, 0, 0, 0, 0]
 
 % --- Jacobian Matrix B(x, u) ---
-% [0, 0, 0, 0]
-% [1 / (- m * cos(x3) ^ 2 + M + m), - (m * sin(x3)) / (m * sin(x3) ^ 2 + M), 0, - (x4 * (- m * x4 * sin(x3) * u4 ^ 2 + bp * cos(x3))) / (u4 ^ 2 * (- m * cos(x3) ^ 2 + M + m))]
-% [0, 0, 0, 0]
-% [-cos(x3) / (u4 * (- m * cos(x3) ^ 2 + M + m)), (m * sin(2 * x3)) / (u4 * (2 * M + m - m * cos(2 * x3))), - (2 * x4) / u4, (2 * M * bp * x4 + 2 * bp * m * x4 - (m ^ 2 * u2 * u4 * sin(2 * x3)) / 2 + 2 * m ^ 2 * u3 * u4 * x4 + m * u1 * u4 * cos(x3) + g * m ^ 2 * u4 * sin(x3) + 2 * M * m * u3 * u4 * x4 - 2 * m ^ 2 * u3 * u4 * x4 * cos(x3) ^ 2 + M * g * m * u4 * sin(x3) - bc * m * u4 * x2 * cos(x3)) / (m * u4 ^ 3 * (- m * cos(x3) ^ 2 + M + m))]
+% [0, 0]
+% [1 / (- m * cos(theta) ^ 2 + M + m), - (m * sin(theta)) / (m * sin(theta) ^ 2 + M)]
+% [0, 0]
+% [-cos(theta) / (l * (- m * cos(theta) ^ 2 + M + m)), (m * sin(2 * theta)) / (l * (2 * M + m - m * cos(2 * theta)))]
+% [0, 0]
+% [0, 1]
 
 % --- Linearized Matrix A at Equilibrium ---
-% [0, 1, 0, 0]
-% [0, -bc / M, (g * m) / M, bp / (M * l0)]
-% [0, 0, 0, 1]
-% [0, bc / (M * l0), - (M * g + g * m) / (M * l0), - (M * bp + bp * m) / (M * l0 ^ 2 * m)]
+% [0, 1, 0, 0, 0, 0]
+% [0, -bc / M, (g * m) / M, bp / (M * l0), 0, 0]
+% [0, 0, 0, 1, 0, 0]
+% [0, bc / (M * l0), - (g * (M + m)) / (M * l0), - (bp * (M + m)) / (M * l0 ^ 2 * m), 0, 0]
+% [0, 0, 0, 0, 0, 1]
+% [0, 0, 0, 0, 0, 0]
 
 % --- Linearized Matrix B at Equilibrium ---
-% [0, 0, 0, 0]
-% [1 / M, 0, 0, 0]
-% [0, 0, 0, 0]
-% [-1 / (M * l0), 0, 0, 0]
+% [0, 0]
+% [1 / M, 0]
+% [0, 0]
+% [-1 / (M * l0), 0]
+% [0, 0]
+% [0, 1]
